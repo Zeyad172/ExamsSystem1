46c914c4e555ad230aad52d05721d1ffe9835510
Noor <nour1999salah@gmail.com>
2025-05-03 10:40:01 +0300

    ypdate

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module org.example.examssystem {
     requires MaterialFX;
     requires java.sql;
-    requires mysql.connector.java;
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
 
 
     opens org.example.examssystem to javafx.fxml;
